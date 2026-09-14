33dcb918711c9ae4f0917508072b74b8afbea22c
Dev Kiddz <denngodfirst@gmail.com>
2026-09-14 17:01:04 +0100

    chore: exclude Laravel runtime files

--- storage/framework/views/0a5b422e8504f69f770834e29addb431.php
+++
@@ -1,8 +0,0 @@
-<script>
-    (() => {
-        const savedTheme = localStorage.getItem('theme');
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        document.documentElement.classList.toggle('dark', savedTheme ? savedTheme === 'dark' : prefersDark);
-    })();
-</script>
-<?php /**PATH C:\xampp\htdocs\tesla.com\resources\views/partials/theme-init.blade.php ENDPATH**/ ?>
